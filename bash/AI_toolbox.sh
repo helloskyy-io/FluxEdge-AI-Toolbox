c8182dda60fe62a3ac524bc828777677dbf8042c
puma <pumapumapumas@gmail.com>
2024-03-30 19:24:53 +0100

    added yaml features

--- bash/AI_toolbox.sh
+++ bash/AI_toolbox.sh
@@ -10,49 +10,55 @@ echo "#     # #   #    #     #      #    ## #        #   "
 echo " #####  #    #   #     #      #     # ######   #   "
 echo ""
 echo "System activated..."
-sleep 2
+sleep 3
 
 # Output description of the script's purpose
 echo "This script will install and configure Ansible on your Ubuntu server container."
 echo "It will also perform system updates using apt update/upgrade."
 echo "Once complete it will prompt you to configure the container packages and dependancies via Ansible."
-sleep 2
+sleep 3
 
 # Prompt user for confirmation to proceed
-read -p "Do you want to continue? (y/n): " choice
+read -p "\n\nDo you want to continue? (y/n): " choice
 if [[ ! $choice =~ ^[Yy]$ ]]; then
     echo "Exiting script."
     exit 1
 fi
 
 # Update package repositories
-echo "Updating package repositories..."
+echo "\n\nUpdating package repositories..."
+sleep 2
 apt update
 
 # Upgrade installed packages
-echo "Upgrading installed packages..."
+echo "\n\nUpgrading installed packages..."
+sleep 2
 # apt upgrade -y
 
 # Install Ansible
-echo "Installing Ansible..."
+echo "\n\nInstalling Ansible..."
+sleep 2
 apt install -y ansible
 
 # Clone Ansible repository
-echo "Cloning FluxEdge-AI-Toolbox..."
+echo "\n\nCloning FluxEdge-AI-Toolbox..."
+sleep 2
 git clone https://github.com/helloskyy-io/FluxEdge-AI-Toolbox.git /FluxEdge_AI_Toolbox
 
 # Display completion message
-echo "Ansible installation and configuration completed successfully."
-echo "You can also run it manually by copying the following command: "
+echo "\n\nAnsible installation and configuration completed successfully."
+sleep 2
+echo "You can run it manually by copying the following command: "
 echo "ansible-playbook -i localhost, -c local /FluxEdge_AI_Toolbox/AI_Toolbox.yml"
 
 # Prompt user for confirmation to proceed
-read -p "Ready to launch Ansible? (y/n): " choice
+read -p "\n\nReady to launch Ansible? (y/n): " choice
 if [[ ! $choice =~ ^[Yy]$ ]]; then
     echo "Exiting script."
     exit 1
 fi
 
 # Launch Ansible playbook
-echo "Launching Ansible playbook..."
-ansible-playbook -i localhost, -c local /FluxEdge_AI_Toolbox/ansible/playbooks/AI_Toolbox.yml
+echo "\n\nLaunching Ansible playbook..."
+sleep 2
+ansible-playbook -i inventories/localhost, -c local /FluxEdge_AI_Toolbox/ansible/playbooks/AI_Toolbox.yml
